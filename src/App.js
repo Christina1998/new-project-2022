9d1ab1d54d3d5a2c6683a1cc08ca5659bb549d48
Christina Shrestha <christnastha18@gmail.com>
2022-07-18 18:21:27 +0545

    Signup form

--- src/App.js
+++ src/App.js
@@ -1,112 +1,133 @@
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Card, Col, Row, Typography } from 'antd';
+import {
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  Typography,
+  Col,
+  Row,
+  Image,
+} from 'antd';
 import '../src/Styles/style.scss';
-import Icon, {
-  SnippetsFilled,
-  CaretUpFilled,
-  CaretDownFilled,
-  MinusOutlined,
-} from '@ant-design/icons';
+import { MailOutlined, LockOutlined } from '@ant-design/icons';
 
 const { Title, Text } = Typography;
-const obj = [
-  {
-    title: 'Data a',
-    count: 340,
-    percent: '50,2%',
-    name: 'John James Bond',
-    icon: (
-      <SnippetsFilled
-        style={{
-          fontSize: 50,
-          float: 'right',
-          color: 'lightcoral',
-        }}
-      />
-    ),
-    percentIcon: <CaretUpFilled style={{ fontSize: 15, marginRight: 5 }} />,
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not a valid email!',
+    number: '${label} is not a valid number!',
   },
-  {
-    title: 'Data b',
-    count: 500,
-    percent: '10,2%',
-    name: 'John James Bond',
-    icon: (
-      <SnippetsFilled
-        style={{
-          fontSize: 50,
-          float: 'right',
-          color: 'lightblue',
-        }}
-      />
-    ),
-    percentIcon: <CaretDownFilled style={{ fontSize: 15, marginRight: 5 }} />,
+  number: {
+    range: '${label} must be between ${min} and ${max}',
   },
-  {
-    title: 'Data c',
-    count: 750,
-    percent: '0,00%',
-    name: 'James Bond',
-    icon: (
-      <SnippetsFilled
-        style={{
-          fontSize: 50,
-          float: 'right',
-          color: 'lightgreen',
-        }}
-      />
-    ),
-    percentIcon: <MinusOutlined style={{ fontSize: 15, marginRight: 5 }} />,
-  },
-  {
-    title: 'Data d',
-    count: 250,
-    percent: '6,2%',
-    name: 'James Bond',
-    icon: (
-      <SnippetsFilled
-        style={{
-          fontSize: 50,
-          float: 'right',
-          color: 'lightgrey',
-        }}
-      />
-    ),
-    percentIcon: <CaretUpFilled style={{ fontSize: 15, marginRight: 5 }} />,
-  },
-];
+};
 
 function App() {
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <Row
-      gutter={{
-        xs: 8,
-        sm: 16,
-        md: 24,
-        lg: 32,
-      }}
-      className="site-card-wrapper"
-    >
-      {obj.map((data) => {
-        return (
-          <Col lg={6}>
-            <Card className="card-wrapper" bordered={false}>
-              {data.icon}
-              <Text type="secondary">{data.title}</Text>
-              <Title level={2} style={{ fontWeight: 'bold', marginTop: 0 }}>
-                {data.count}
-              </Title>
-              <Title level={4} style={{ color: 'lightgreen', marginTop: 0 }}>
-                {data.percentIcon}
-                {data.percent}
-              </Title>
-              <Text type="secondary">{data.name}</Text>
-            </Card>
-          </Col>
-        );
-      })}
+    <Row>
+      <Col span={16}>
+        <header className="App-header">
+          <Title level={3} style={{ fontWeight: 'bold' }}>
+            Sign Up
+          </Title>
+          <Text type="secondary" style={{ fontSize: 13, textAlign: 'left' }}>
+            Already have an account? Sign In
+          </Text>
+          <Form
+            name="basic"
+            labelCol={{ span: 12 }}
+            className="form"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <label required>Email</label>
+            <Form.Item
+              name="email"
+              prefix={<MailOutlined className="item-icon" />}
+              rules={[
+                { required: true, message: 'Please input your email!' },
+                { type: 'email' },
+                { min: 3 },
+                { whitespace: true },
+              ]}
+              className="input_label"
+            >
+              <Input
+                prefix={<MailOutlined className="item-icon" />}
+                className="input"
+              />
+            </Form.Item>
+            <label required>Password</label>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: 'Please input your password!' },
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined className="item-icon" />}
+                className="input"
+              />
+            </Form.Item>
+            <label>Confirm Password</label>
+
+            <Form.Item
+              name="confirm_password"
+              dependencies={['password']}
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password again!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      'The password you entered doesnot match'
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined className="item-icon" />}
+                className="input"
+              />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type="primary" htmlType="submit" className="button">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </header>
+      </Col>
+      <Col span={8}>
+        <Image className="image" preview={{ visible: false }} src="/pic.png" />
+      </Col>
     </Row>
   );
 }
